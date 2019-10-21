a758ec9676b2bf0620d6d1fd215fbd547dedd9fe
YanKohut <kohutyan@gmail.com>
2019-10-21 17:52:38 +0300

    Initial commit

---
+++ app/src/main/java/com/kohutyan/homework02/MainActivity.java
@@ -0,0 +1,78 @@
+package com.kohutyan.homework02;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private EditText mEditTextNumber1;
+    private EditText mEditTextNumber2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        mEditTextNumber1 = findViewById(R.id.edit_text_number1);
+        mEditTextNumber2 = findViewById(R.id.edit_text_number2);
+
+        Button buttonOpenActivity2 = findViewById(R.id.button_open_activity2);
+        buttonOpenActivity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mEditTextNumber1.getText().toString().equals("")
+                        || mEditTextNumber2.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, "Please insert numbers", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    int number1 = Integer.parseInt(mEditTextNumber1.getText().toString());
+                    int number2 = Integer.parseInt(mEditTextNumber2.getText().toString());
+
+                    Intent intent = new Intent(MainActivity.this, Activity2.class);
+                    intent.putExtra("number1", number1);
+                    intent.putExtra("number2", number2);
+                    startActivityForResult(intent, 1);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                int result = data.getIntExtra("result", 0);
+                mTextViewResult.setText("" + result);
+            }
+            if (resultCode == RESULT_CANCELED) {
+                mTextViewResult.setText("Nothing selected");
+            }
+        }
+    }
+
+
+
+    public void intent(View view) {
+        Intent intent = new Intent (Intent.ACTION_SEND);
+        intent.setType("message/rfc822");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"anyMail@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Any subject if you want");
+        intent.setPackage("com.google.android.gm");
+        if (intent.resolveActivity(getPackageManager())!=null)
+            startActivity(intent);
+        else
+            Toast.makeText(this,"Gmail App is not installed",Toast.LENGTH_SHORT).show();
+
+    }
+}
